9977cb7e8261132f806f5e45d45c85132fdc0b40
Cornelius Horstmann <horstmann@tbointeractive.com>
2017-10-27 19:08:24 +0200

    Added automatic generation of the url property

--- PiwikTracker/Application.swift
+++ PiwikTracker/Application.swift
@@ -7,16 +7,20 @@ internal struct Application {
     static func makeCurrentApplication() ->  Application {
         let displayName = bundleDisplayNameForCurrentApplication()
         let name = bundleNameForCurrentApplication()
+        let identifier = bundleIdentifierForCurrentApplication()
         let version = bundleVersionForCurrentApplication()
         let shortVersion = bundleShortVersionForCurrentApplication()
-        return Application(bundleDisplayName: displayName, bundleName: name, bundleVersion: version, bundleShortVersion: shortVersion)
+        return Application(bundleDisplayName: displayName, bundleName: name, bundleIdentifier: identifier, bundleVersion: version, bundleShortVersion: shortVersion)
     }
     
     /// The name of your app as displayed on the homescreen i.e. "My App"
     let bundleDisplayName: String?
     
-    /// The bundle name of your app i.e. "com.my-company.my-app"
+    /// The bundle name of your app i.e. "my-app"
     let bundleName: String?
+    
+    /// The bundle identifier of your app i.e. "com.my-company.my-app"
+    let bundleIdentifier: String?
     
     /// The bundle version a.k.a. build number as String i.e. "149"
     let bundleVersion: String?
@@ -32,6 +36,11 @@ internal struct Application {
     /// Returns the bundle name of the app
     private static func bundleNameForCurrentApplication() -> String? {
         return Bundle.main.infoDictionary?["CFBundleName"] as? String
+    }
+    
+    /// Returns the bundle identifier
+    private static func bundleIdentifierForCurrentApplication() -> String? {
+        return Bundle.main.infoDictionary?["CFBundleIdentifier"] as? String
     }
     
     /// Returns the bundle version
